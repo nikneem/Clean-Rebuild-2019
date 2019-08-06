21115b52d0edf84e13137258bbfcb58296642007
Eduard Keilholz <eduard.keilholz@loddersoftware.com>
2019-08-06 10:10:47 +0200

    Refactored and neaten code

--- CleanRebuild2019/CleanRebuildCommand.cs
+++ CleanRebuild2019/CleanRebuildCommand.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.ComponentModel.Design;
-using System.Globalization;
-using System.Threading;
 using System.Threading.Tasks;
 using EnvDTE;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using Task = System.Threading.Tasks.Task;
+using Threading = System.Threading.Tasks;
 
 namespace CleanRebuild2019
 {
@@ -77,7 +74,7 @@ namespace CleanRebuild2019
         /// Initializes the singleton instance of the command.
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
-        public static async Task InitializeAsync(AsyncPackage package)
+        public static async Threading.Task InitializeAsync(AsyncPackage package)
         {
             // Switch to the main thread - the call to AddCommand in CleanRebuildCommand's constructor requires
             // the UI thread.
@@ -85,18 +82,21 @@ namespace CleanRebuild2019
 
             OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
             Instance = new CleanRebuildCommand(package, commandService);
-            await Instance.Initialize();
+            Instance._dte = await Instance.GetServiceByTypeAsync<DTE>();
+            Instance._dte.Events.SolutionEvents.Opened += Instance.SolutionOpenendEventHandler;
+            Instance._dte.Events.SolutionEvents.BeforeClosing += Instance.SolutionClosingEventHandler;
         }
 
-        private async Task Initialize()
+
+        private void SolutionOpenendEventHandler()
         {
-            if (_dte == null)
-            {
-                _dte = await GetServiceByTypeAsync<EnvDTE.DTE>();
-                _dte.Events.SolutionEvents.Opened += SolutionEvents_Opened;
-                _dte.Events.SolutionEvents.AfterClosing += SolutionEvents_AfterClosing;
-            }
+            _command.Enabled = true;
         }
+        private void SolutionClosingEventHandler()
+        {
+            _command.Enabled = false;
+        }
+
 
 
 
@@ -109,25 +109,14 @@ namespace CleanRebuild2019
         /// <param name="e">Event args.</param>
         private async void Execute(object sender, EventArgs e)
         {
-            if (_dte.Solution != null && _dte.Solution.Count >0)
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+            if (_dte.Solution != null && _dte.Solution.Count > 0)
             {
                 _dte.Solution.SolutionBuild.Clean(true);
-                foreach (Project project in _dte.Solution.Projects)
-                {
-                    _dte.Solution.SolutionBuild.BuildProject(_dte.Solution.SolutionBuild.ActiveConfiguration.Name,
-                        project.UniqueName, true);
-                }
+                _dte.Solution.SolutionBuild.Build();
             }
         }
 
-        private void SolutionEvents_Opened()
-        {
-            _command.Enabled = true;
-        }
-        private void SolutionEvents_AfterClosing()
-        {
-            _command.Enabled = false;
-        }
 
         /// <summary>
         /// Represents the method that
